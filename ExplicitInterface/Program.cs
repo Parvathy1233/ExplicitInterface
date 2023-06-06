a8cf6d0bdae9db8005d46115d9a77728b55736e2
Sreelakshmi Sabu <Sreelakshmi.Sabu@oneteam.us>
2023-06-06 10:05:45 +0530

    Add project files.

---
+++ ExplicitInterface/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Method 1
+            MediaPlayer mediaPlayer=new MediaPlayer();
+            ((IVideo)mediaPlayer).VideoPlayer();
+            ((IAudio)mediaPlayer).AudioPlayer();
+            Console.ReadLine();
+
+            //Method 2
+            IVideo video = new MediaPlayer();
+            IAudio audio = new MediaPlayer();
+            video.VideoPlayer();
+            audio.AudioPlayer();
+            Console.ReadLine();
+        }
+    }
+}
+public interface IVideo
+{
+    void VideoPlayer();
+}
+
+public interface IAudio
+{
+    void AudioPlayer();
+}
+public class MediaPlayer : IVideo, IAudio
+{
+    void IAudio.AudioPlayer()
+    {
+        Console.WriteLine("Audio-Player is working.");
+    }
+    void IVideo.VideoPlayer()
+    {
+        Console.WriteLine("Video-Player is working");
+    }
+}
